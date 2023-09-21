8ccf3c5c28b1dac40fb72dedfeae0d7c20ce937f
JTuratkhan <108810678+JTuratkhan@users.noreply.github.com>
2023-09-20 21:50:26 -0400

    Create test.py

---
+++ test/test.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+from ydata_profiling import ProfileReport
+import polars as pl
+
+house_data = "house.csv"
+
+def house_statistics_polars():
+    polars_house_df = pl.read_csv(house_data)
+
+    # Calculate average prices
+    max_ave_prices_df = polars_house_df.select(pl.max("price")).to_pandas()["max"]
+
+    # Select rows with the highest average prices
+    polars_prices_df = polars_house_df.filter(pl.col("price") == max_ave_prices_df)
+
+    # Display houses dataset statistics
+    print('Summary Statistics of the house prices:\n')
+    print(polars_house_df.describe())
+
+    # Generate plot for metro distance vs price
+    plt.scatter(polars_house_df["metro_distance"].to_pandas(), polars_house_df["price"].to_pandas())
+    plt.title("Metro Distance vs Price")
+    plt.xlabel("Metro Distance")
+    plt.ylabel("Average Price")
+    plt.show()
+
+    # Display details about the highest priced houses
+    print("\nDetails of the highest priced houses are: \n")
+    print(polars_prices_df)
+
+    # Generate a .html summary report
+    polars_report_generator(polars_house_df)
+    return polars_prices_df
+
+def polars_report_generator(polars_house_df):
+    profile = ProfileReport(polars_house_df.to_pandas(), title="Summary Report")
+    profile.to_file("Polars_Summary_Report.html")
+
+house_statistics_polars()
